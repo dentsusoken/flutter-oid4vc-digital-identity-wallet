c52297b6a955ae9eb96d5d6fe8e368a27a822700
maku <maku.sho@dentsusoken.com>
2025-03-26 10:46:09 +0900

    Add VP history providers and exports for enhanced state management and data handling

---
+++ lib/features/vp/presentation/provider/vp_history_details_provider.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'vp_history_details_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$vpHistoryDetailsHash() => r'0fbf846a09ff2c8f7a25b613a37b57926be58f2e';
+
+/// See also [VpHistoryDetails].
+@ProviderFor(VpHistoryDetails)
+final vpHistoryDetailsProvider = AutoDisposeNotifierProvider<VpHistoryDetails, PresentationLog?>.internal(
+  VpHistoryDetails.new,
+  name: r'vpHistoryDetailsProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product') ? null : _$vpHistoryDetailsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$VpHistoryDetails = AutoDisposeNotifier<PresentationLog?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
