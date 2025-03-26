a867b20602ea380a1d1e3c25c33faa631ed30e4f
maku <maku.sho@dentsusoken.com>
2025-03-26 10:43:36 +0900

    Refactor date formatting method in DateTime extension for improved readability

--- lib/core/extensions/date_extension.dart
+++ lib/core/extensions/date_extension.dart
@@ -3,9 +3,7 @@ import 'package:intl/intl.dart';
 extension DateTimeExt on DateTime {
   static const _defaultFormat = 'yyyy-MM-dd HH:mm:ss.SSS';
 
-  String format({
-    String? format,
-  }) {
+  String format({String? format}) {
     return DateFormat(format ?? _defaultFormat).format(this);
   }
 }
