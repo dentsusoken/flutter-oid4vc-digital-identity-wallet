dc51dcc576c63a434e97f566c08e892afb5c6bea
maku <maku.sho@dentsusoken.com>
2025-03-26 10:44:35 +0900

    Refactor text style and theme configuration for improved readability and consistency

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -9,32 +9,19 @@ class AppTheme {
   static const mode = ThemeMode.light;
 
   static final _buttonStyle = ButtonStyle(
-    padding: WidgetStateProperty.all(
-      const EdgeInsets.symmetric(
-        vertical: 12,
-        horizontal: 16,
-      ),
-    ),
+    padding: WidgetStateProperty.all(const EdgeInsets.symmetric(vertical: 12, horizontal: 16)),
     elevation: WidgetStateProperty.all(6),
   );
 
   static final ThemeData light = ThemeData(
     primarySwatch: Colors.blue,
     appBarTheme: AppBarTheme(
-      iconTheme: const IconThemeData(
-        color: AppColors.black,
-      ),
+      iconTheme: const IconThemeData(color: AppColors.black),
       backgroundColor: AppColors.lightBlue,
       centerTitle: true,
-      titleTextStyle: AppTextStyle.h6.copyWith(
-        color: AppColors.black,
-      ),
+      titleTextStyle: AppTextStyle.h6.copyWith(color: AppColors.black),
     ),
-    outlinedButtonTheme: OutlinedButtonThemeData(
-      style: _buttonStyle,
-    ),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: _buttonStyle,
-    ),
+    outlinedButtonTheme: OutlinedButtonThemeData(style: _buttonStyle),
+    elevatedButtonTheme: ElevatedButtonThemeData(style: _buttonStyle),
   );
 }
